1cba47944604eabce085243ed05cbf2db5dcc722
OjasKataria <63237038+OjasKataria@users.noreply.github.com>
2021-07-10 10:59:12 +0530

    forgot to update bot.py

--- Bot.py
+++ Bot.py
@@ -1,7 +1,7 @@
 import os
 import discord
 from discord.ext import commands
-from cogs.music import Music
+from cogs import hangman, knockjokes, music
 
 TOKEN = os.environ['TOKEN']
 
@@ -14,27 +14,6 @@ bot = commands.Bot(command_prefix="$", intents=intents)
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
-
-# @bot.command()
-# async def hangman(ctx):
-#     if ctx.author.dm_channel == None:
-#         await ctx.author.create_dm()
-#     await ctx.author.dm_channel.send("Please respond with a phrase you would like to use for your hangman game in **{.guild}**. \n\nPlease keep phrases less than 31 characters".format(ctx))
-#     await ctx.reply("Sent you a dm! Please respond there with the phrase you would like to setup")
-
-#     def check(m):
-#         return m.channel == ctx.author.dm_channel and m.author == ctx.author and len(m.content) < 31
-    
-#     msg = await bot.wait_for('message', check=check, timeout=60)
-#     word = msg.content
-#     await ctx.send("Alright, a hangman game has just started, you can start guessing now!\n```     ——\n    |  |\n       |\n       |\n       |\n       |\n       |\n    ———————```")
-#     await ctx.send("```Guesses:```")
-
-
-# @hangman.error
-# async def count_error(ctx, error):
-#     if isinstance(error, discord.ext.commands.errors.CommandInvokeError):
-#         await ctx.send("You took too long! Please look at your DM's as that's where I'm asking for the phrase you want to use")
     
 
 @bot.event
@@ -56,6 +35,8 @@ async def on_message(message):
 
     await bot.process_commands(message)
 
-bot.add_cog(Music(bot))
+bot.add_cog(music.Music(bot))
+bot.add_cog(hangman.Hangman(bot))
+bot.add_cog(knockjokes.jokes(bot))
 
 bot.run(TOKEN)
