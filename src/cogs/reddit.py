f387e99b86af884744f0a711253570f265d29d1c
Ojas Kataria <63237038+IrishBumfuzzle@users.noreply.github.com>
2022-05-01 15:24:36 +0530

    Now only one function needed

--- src/cogs/reddit.py
+++ src/cogs/reddit.py
@@ -9,20 +9,19 @@ class FreeGameFindings(commands.Cog):
     def __init__(self, bot, channel_id) -> None:
         self.bot = bot
         self.channel_id = channel_id
-        self.check_for_new_posts_epic.start()
-        self.check_for_new_posts_steam.start()
+        self.check_for_new_posts.start()
 
     @tasks.loop(hours=1)
-    async def check_for_new_posts_epic(self):
+    async def check_for_new_posts(self):
         d = feedparser.parse(
-            "https://www.reddit.com/r/FreeGameFindings/search.rss?q=title%3Aepicgames%20OR%20title%3Aepic&restrict_sr=on&sort=new&t=all"
+            "https://www.reddit.com/r/FreeGameFindings/search.rss?q=title%3Aepicgames%20OR%20title%3Aepic%20OR%20title%3Asteam&restrict_sr=on&sort=new&t=all"
         )
 
         try:
-            with open("last_time_epic.data", "rb") as f:
+            with open("last_time.data", "rb") as f:
                 last_time = pickle.load(f)
         except:
-            with open("last_time_epic.data", "wb") as f:
+            with open("last_time.data", "wb") as f:
                 last_time = time.mktime(time.gmtime())
                 pickle.dump(last_time, f)
 
@@ -32,28 +31,5 @@ class FreeGameFindings(commands.Cog):
                 print(i.link)
                 channel = await self.bot.fetch_channel(self.channel_id)
                 await channel.send(i.link)
-                with open("last_time_epic.data", "wb") as f:
-                    pickle.dump(pub_time, f)
-
-    @tasks.loop(hours=1)
-    async def check_for_new_posts_steam(self):
-        d = feedparser.parse(
-            "https://www.reddit.com/r/FreeGameFindings/search.rss?q=title%3Asteam&restrict_sr=on&sort=new&t=all"
-        )
-
-        try:
-            with open("last_time_steam.data", "rb") as f:
-                last_time = pickle.load(f)
-        except:
-            with open("last_time_steam.data", "wb") as f:
-                last_time = time.mktime(time.gmtime())
-                pickle.dump(last_time, f)
-
-        for i in d.entries[::-1]:
-            pub_time = time.mktime(i.published_parsed)
-            if pub_time > last_time:
-                print(i.link)
-                channel = await self.bot.fetch_channel(self.channel_id)
-                await channel.send(i.link)
-                with open("last_time_steam.data", "wb") as f:
+                with open("last_time.data", "wb") as f:
                     pickle.dump(pub_time, f)
